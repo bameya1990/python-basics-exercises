468c484bc648318a4a2fcbe0df101cedfee76c8b
Ameya Bongale <bameya1990+github@gmail.com>
2024-04-29 14:46:21 -0400

    continue

---
+++ continue.py
@@ -0,0 +1,9 @@
+cities = ['Istanbul', 'Los Angeles', 'Tokyo', None,
+          'Vienna', None, 'London', 'Beijing', None]
+
+
+for city in cities:
+    if city != None: 
+        print(len(city))
+    else:
+        continue
